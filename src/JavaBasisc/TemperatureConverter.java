ab70aa860e5b1f8a3344f5ff88cd800463efd6c4
Alessio-Tempesta <aletem999@gmail.com>
2025-01-21 16:43:47 +0100

    Add an exercise Temperature Converter in Java

---
+++ src/JavaBasisc/TemperatureConverter.java
@@ -0,0 +1,24 @@
+package JavaBasisc;
+import java.util.Scanner;
+
+public class TemperatureConverter {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double temp;
+        double newTemp;
+        String unit;
+
+        System.out.println("Enter the temperature");
+        temp = scanner.nextDouble();
+
+        System.out.print("Converter to Celsius or Fahrenheit? (C or F)");
+        unit = scanner.next().toUpperCase();
+
+        newTemp = (unit.equals("C")) ? (temp - 32) * 5 / 9 : (temp * 5 / 9 ) + 32;
+        System.out.printf("%.1f°%s", newTemp, unit);
+
+        scanner.close();
+    }
+}
