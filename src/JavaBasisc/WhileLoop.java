cf3eb7bc55c1005f5add71727ce04e44a9296b62
Alessio-Tempesta <aletem999@gmail.com>
2025-01-22 16:40:14 +0100

    Add other excercise in Java With do-While or While loop

--- src/JavaBasisc/WhileLoop.java
+++ src/JavaBasisc/WhileLoop.java
@@ -8,15 +8,27 @@ public class WhileLoop {
 
         Scanner scanner = new Scanner(System.in);
 
-        int age = 0;
-        do {
-            System.out.println("Your age can't be negative");
-            System.out.print("enter your age: ");
-            age = scanner.nextInt();
-        }while (age < 0);
-         System.out.println("You are " + age + "years old");
+        int num = 0;
 
+        do {
+            System.out.print("Enter a number beetween 1 - 10 ");
+            num = scanner.nextInt();
+        }
+        while (num < 1 || num > 10);
+
+        System.out.println("You picked" + num);
         scanner.close();
+
+
+
+//        int age = 0;
+//        do {
+//            System.out.println("Your age can't be negative");
+//            System.out.print("enter your age: ");
+//            age = scanner.nextInt();
+//        }while (age < 0);
+//         System.out.println("You are " + age + "years old");
+
 
 //        String response ="";
 //
