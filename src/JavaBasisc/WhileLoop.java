5eacd0ddc28a560579b2a37cf08ab0628af93d4c
Alessio-Tempesta <aletem999@gmail.com>
2025-01-22 16:13:08 +0100

    Add another exercise with Do-While

--- src/JavaBasisc/WhileLoop.java
+++ src/JavaBasisc/WhileLoop.java
@@ -8,16 +8,25 @@ public class WhileLoop {
 
         Scanner scanner = new Scanner(System.in);
 
-        String response ="";
-
-        while(!response.equals("Q")){
-            System.out.println("You are a playing a game");
-            System.out.print("Press Q to quit");
-            response = scanner.next().toUpperCase();
-        }
-        System.out.println("You have quit the game ");
+        int age = 0;
+        do {
+            System.out.println("Your age can't be negative");
+            System.out.print("enter your age: ");
+            age = scanner.nextInt();
+        }while (age < 0);
+         System.out.println("You are " + age + "years old");
 
         scanner.close();
+
+//        String response ="";
+//
+//        while(!response.equals("Q")){
+//            System.out.println("You are a playing a game");
+//            System.out.print("Press Q to quit");
+//            response = scanner.next().toUpperCase();
+//        }
+//        System.out.println("You have quit the game ");
+
 
 //        while (name.isEmpty()){
 //            System.out.print(" Enter Your name ");
