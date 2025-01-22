c9c09cea70b6a19464e5fbcb92c48a02d7d5cb81
Alessio-Tempesta <aletem999@gmail.com>
2025-01-22 22:50:45 +0100

    Add the Exercise With ForLoop HappyNew Year +Thread.sleep()

--- src/JavaBasisc/ForLoops.java
+++ src/JavaBasisc/ForLoops.java
@@ -1,21 +1,38 @@
 package JavaBasisc;
 
 import java.util.Scanner;
+import java.util.concurrent.ThreadPoolExecutor; // --> Thread.sleep(): Questo è un metodo statico della classe Thread in Java.
+// "Statico" significa che puoi chiamarlo direttamente dalla classe senza dover creare un'istanza di un oggetto Thread.
+//1000: Questo valore numerico rappresenta il tempo di pausa in millisecondi.
+
+//       Puoi specificare qualsiasi valore intero positivo per definire la durata della pausa.
 
 public class ForLoops {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 //        Il ciclo for è uno strumento fondamentale in Java che ci permette di eseguire ripetutamente
 //        un blocco di codice un numero specificato di volte.
 //        È particolarmente utile quando sappiamo in anticipo quante iterazioni vogliamo effettuare.
+//
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter how many times you wnat to loop ");
-        int max = scanner.nextInt();
+        System.out.println("How many secodns to countDown from ?:  " );
+        int start = scanner.nextInt();
 
-        for (int i =0; i <= max; i++ ){
+        for (int i = start; i > 0; i--){
             System.out.println(i);
+            Thread.sleep(1000);
         }
+        System.out.println(" HAPPY NEW YEAR ");
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter how many times you wnat to loop ");
+//        int max = scanner.nextInt();
+//
+//        for (int i =0; i <= max; i-- ){
+//            System.out.println(i);
+//        }
 
     }
 }
