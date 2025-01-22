cb2f23e01d3b2e7ce8092522332211e7f7f2d248
Alessio-Tempesta <aletem999@gmail.com>
2025-01-22 16:43:05 +0100

    Add inizialitzed a class NumberGuessingGame in Java

---
+++ src/JavaBasisc/NumberGuessingGame.java
@@ -0,0 +1,15 @@
+package JavaBasisc;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+
+        //NumberGuessingGame
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+    }
+}
