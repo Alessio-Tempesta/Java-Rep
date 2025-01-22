792c555aded203da45d1d4b333c1c2c2c2193f25
Alessio-Tempesta <aletem999@gmail.com>
2025-01-22 16:04:13 +0100

    Add another excercise With While loop in java

--- src/JavaBasisc/WhileLoop.java
+++ src/JavaBasisc/WhileLoop.java
@@ -1,20 +1,30 @@
 package JavaBasisc;
-import java.awt.desktop.SystemEventListener;
+import java.util.Locale;
 import java.util.Scanner;
 
 
 public class WhileLoop {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        String name ="";
+        String response ="";
 
-        while (name.isEmpty()){
-            System.out.print(" Enter Your name ");
-            name = scanner.nextLine();
-    }
-        System.out.println(" Hello " + name);
+        while(!response.equals("Q")){
+            System.out.println("You are a playing a game");
+            System.out.print("Press Q to quit");
+            response = scanner.next().toUpperCase();
+        }
+        System.out.println("You have quit the game ");
 
         scanner.close();
+
+//        while (name.isEmpty()){
+//            System.out.print(" Enter Your name ");
+//            name = scanner.nextLine();
+//    }
+//        System.out.println(" Hello " + name);
+
+
     }
 }
