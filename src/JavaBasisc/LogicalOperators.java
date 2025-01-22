860941551b886d43c84c7ab5eb1d5c531dd32c12
Alessio-Tempesta <aletem999@gmail.com>
2025-01-22 15:51:22 +0100

    Add an exercise Username with logical operator

--- src/JavaBasisc/LogicalOperators.java
+++ src/JavaBasisc/LogicalOperators.java
@@ -1,5 +1,7 @@
 package JavaBasisc;
 
+import java.util.Scanner;
+
 public class LogicalOperators {
     public static void main(String[] args) {
         //&& (AND):
@@ -17,18 +19,41 @@ public class LogicalOperators {
         // Se la condizione è vera, ! la rende falsa.
         // Se la condizione è falsa, ! la rende vera.
 
-        double temp = 0;
-        boolean isSunny = false;
+//
+//        double temp = 0;
+//        boolean isSunny = false;
+//
+//        if(temp <= 30 && temp >= 0 && isSunny){
+//            System.out.println("The weather is GOOD");
+//            System.out.println("It is SUNNY outside");
+//        }
+//        else if (temp >= 30 && temp >= 0 && !isSunny) {
+//            System.out.println("The weather is GOOD");
+//            System.out.println("It is CLOUDY outside");
+//        } else if (temp > 30 || temp < 0 ) {
+//            System.out.println("The weather is bad");
+//        }
 
-        if(temp <= 30 && temp >= 0 && isSunny){
-            System.out.println("The weather is GOOD");
-            System.out.println("It is SUNNY outside");
+
+        Scanner scanner = new Scanner(System.in);
+
+//        username must be between- characters
+//        username must not contain spaces or underscores
+
+        String username;
+
+        System.out.print("Enter your new username");
+        username = scanner.nextLine();
+
+        if (username.length() < 4 || username.length() > 12){
+            System.out.println("Username must be between 4-12 characters");
         }
-        else if (temp >= 30 && temp >= 0 && !isSunny) {
-            System.out.println("The weather is GOOD");
-            System.out.println("It is CLOUDY outside");
-        } else if (temp > 30 || temp < 0 ) {
-            System.out.println("The weather is bad");
+        else if(username.contains(" ") || username.contains("_")){
+            System.out.print("Username must not contain spaces or _ underscore");
         }
+        else {
+            System.out.println("Welcome "+ username);
+        }
+        scanner.close();
     }
 }
