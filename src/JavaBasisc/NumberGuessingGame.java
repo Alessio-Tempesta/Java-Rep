bf71484760a19c17de0099fdcbafa0e23ccdb1cd
Alessio-Tempesta <aletem999@gmail.com>
2025-01-22 22:23:55 +0100

    Try and play a game of the NumberGuessing in Java

--- src/JavaBasisc/NumberGuessingGame.java
+++ src/JavaBasisc/NumberGuessingGame.java
@@ -11,5 +11,34 @@ public class NumberGuessingGame {
 
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
+
+        int guess;
+        int attempts = 0;
+        int min = 1;
+        int max = 100;
+        int randomNumber = random.nextInt(min, max + 1);
+
+        System.out.println("Number Guessing Game ");
+        System.out.printf("Guess a number between %d-%d\n ", min, max);
+
+        do {
+            System.out.print("Enter a guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < randomNumber){
+                System.out.println("Too low! TRY AGAIN");
+            }
+            else if (guess > randomNumber){
+                System.out.println("TOO HIGH! Try again");
+            }
+            else {
+                System.out.println("CORRECT! the number was " + randomNumber );
+                System.out.println("# of attempts: " + attempts);
+            }
+            
+        }while (guess != randomNumber);
+
+        scanner.close();
     }
 }
