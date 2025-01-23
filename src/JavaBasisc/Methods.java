45a5e271549cc6df5b9532cf0f172d821cc14220
Alessio-Tempesta <aletem999@gmail.com>
2025-01-23 14:46:07 +0100

    init to metodh in java

--- src/JavaBasisc/Methods.java
+++ src/JavaBasisc/Methods.java
@@ -1,7 +1,27 @@
+//I Metodi in Java:
+//Cosa sono i metodi?
+//Immagina i metodi come delle piccole funzioni all'interno del tuo programma Java.
+// Ognuno di essi ha un compito specifico e può essere richiamato più volte nel codice.
+// Pensali come delle "ricette" che ti dicono come eseguire una determinata operazione.
+//Perché usare i metodi?
+//Organizzazione del codice: Dividendo il codice in metodi più piccoli, lo rendi più leggibile e facile da mantenere.
+//Riutilizzabilità: Un metodo può essere chiamato da diverse parti del tuo programma,
+//evitando di riscrivere lo stesso codice più volte.
+//Modularità: Ogni metodo si concentra su un singolo compito, rendendo il codice più modulare e facile da testare.
+
 package JavaBasisc;
 
 public class Methods {
     public static void main(String[] args) {
+
+//        Method => sono dei blocchi di codice che possono essere eseguti e riutilizzati tramite delle chiamate => ()
+
+        String name "Bro";
+        int age = 25;
+
+
+    }
+    static void happyBirthday(){
 
     }
 }
