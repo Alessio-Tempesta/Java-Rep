99b11a79c1062867bd307720edb37d2f128d56d6
Alessio-Tempesta <aletem999@gmail.com>
2025-01-23 17:13:21 +0100

    Add another exercise Overloaded Methods

--- src/Methods/OverLoadedMethods.java
+++ src/Methods/OverLoadedMethods.java
@@ -15,22 +15,36 @@ package Methods;
 public class OverLoadedMethods {
     public static void main(String[] args) {
 
-        // first Example :
-
-        System.out.println(add(1, 2));
-        System.out.println(add(1, 2, 3));
-        System.out.println(add(1, 2, 3, 4));
-
+        String pizza = bakePizza("Flat bread", "Mozzarella", "peperoni");
+        System.out.println(pizza);
     }
-
-    static double add(double a, double b) {
-        return a + b;
+    static String bakePizza(String bread){
+        return bread + " Pizza";
     }
-
-    static double add(double a, double b, double c) {
-        return a + b + c;
+    static String bakePizza(String bread, String cheese){
+        return cheese + " " + bread + " pizza";
     }
-    static double add(double a, double b, double c, double d) {
-        return a + b + c + d;
+    static String bakePizza(String bread, String cheese, String topping){
+        return topping + " " + cheese + " " + bread + " pizza";
     }
 }
+
+        // first Example :
+
+//        System.out.println(add(1, 2));
+//        System.out.println(add(1, 2, 3));
+//        System.out.println(add(1, 2, 3, 4));
+
+
+
+//    static double add(double a, double b) {
+//        return a + b;
+//    }
+//
+//    static double add(double a, double b, double c) {
+//        return a + b + c;
+//    }
+//    static double add(double a, double b, double c, double d) {
+//        return a + b + c + d;
+//    }
+
