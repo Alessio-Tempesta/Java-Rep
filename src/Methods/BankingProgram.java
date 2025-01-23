ae750600d1ad9b32077671dae297da229f8a1e99
Alessio-Tempesta <aletem999@gmail.com>
2025-01-23 23:01:52 +0100

    Add an Methods showBalance() & Desposit()

--- src/Methods/BankingProgram.java
+++ src/Methods/BankingProgram.java
@@ -3,11 +3,12 @@ package Methods;
 import java.util.Scanner;
 
 public class BankingProgram {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         // Banking program:
 
-        Scanner scanner = new Scanner(System.in);
-        double balance;
+
+        double balance = 0;
         boolean isRunning = true;
         int choice;
 
@@ -25,8 +26,8 @@ public class BankingProgram {
             choice = scanner.nextInt();
 
             switch (choice){
-                case 1 -> System.out.println(" SHOW BALANCE ");
-                case 2 -> System.out.println(" DEPOSIT ");
+                case 1 -> showBalance(balance);
+                case 2 -> balance = balance += deposit();
                 case 3 -> System.out.println(" WITHDRAW ");
                 case 4 -> isRunning = false;
                 default -> System.out.println("INVALID CHOICE");
@@ -45,6 +46,22 @@ public class BankingProgram {
     }
 
     static void showBalance(double balance){
-        System.out.println();
+        System.out.println("*****************");
+        System.out.printf("€%.2f\n", balance);
+    }
+    static double deposit(){
+
+    double amount;
+
+        System.out.print("Enter an amount to be deposited : ");
+        amount = scanner.nextDouble();
+
+        if (amount < 0){
+            System.out.println("Amount can't be negative :");
+            return 0;
+        }
+        else {
+            return amount;
+        }
     }
 }
