b7e90c60dabb2bcac0f776ed6d319be1a5f7867e
Alessio-Tempesta <aletem999@gmail.com>
2025-01-26 17:12:44 +0100

    Array 2 in java init

---
+++ src/Array/Arrays2d.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class Arrays2d {
+    public static void main(String[] args) {
+
+//        Cosa sono gli Array 2D?
+//Un array bidimensionale (2D) è una struttura dati che rappresenta una "tabella" o una "matrice" con righe e colonne.
+// In Java, un array 2D è essenzialmente un array di array.
+//Sintassi: Tipo[][] nomeArray = new Tipo[righe][colonne];
+//Tipo: Il tipo di dati degli elementi (es. int, String, ecc.).
+//righe: Numero di righe.
+//colonne: Numero di colonne.
+
+    }
+}
