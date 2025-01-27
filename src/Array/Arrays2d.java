96cd8274ea238d55bb034e62508e7947d8b1bdfa
Alessio-Tempesta <aletem999@gmail.com>
2025-01-27 23:53:23 +0100

    add an exercise With array 2D Char and Number of telephone

--- src/Array/Arrays2d.java
+++ src/Array/Arrays2d.java
@@ -10,21 +10,34 @@ public class Arrays2d {
 //Tipo: Il tipo di dati degli elementi (es. int, String, ecc.).
 //righe: Numero di righe.
 //colonne: Numero di colonne.
+    char[][] telephone = {{'1','2','3'},
+                          {'4','5','6'},
+                          {'7','8','9'},
+                          {'*','0','#'}};
+    for (char[] row : telephone){
+        for (char number : row){
+            System.out.print(number + " ");
+        }
+        System.out.println();
+    }
 
 
-        String[] fruits = {"apple", "orange", "banana"};
-        String[] vegetables = {"potato", "onion", "carrot"};
-        String[] meats = {"chicken","pork", "beef", "fish"};
 
-        String[][] groceries = {{"apple", "orange", "banana"},
-                                {"potato", "onion", "carrot"},
-                                {"chicken","pork", "beef", "fish"}};
 
-        for (String [] foods : groceries){
-            for (String food : foods){
-                System.out.print(food + " ");
-            }
-            System.out.println();
+
         }
     }
-}
+
+//        String[] fruits = {"apple", "orange", "banana"};
+//        String[] vegetables = {"potato", "onion", "carrot"};
+//        String[] meats = {"chicken","pork", "beef", "fish"};
+//
+//        String[][] groceries = {{"apple", "orange", "banana"},
+//                                {"potato", "onion", "carrot"},
+//                                {"chicken","pork", "beef", "fish"}};
+//
+//        for (String [] foods : groceries){
+//            for (String food : foods){
+//                System.out.print(food + " ");
+//            }
+//            System.out.println();
