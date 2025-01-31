efe801e3fa923bbe03b39f7cbd9c93da0e6c8149
Alessio Tempesta <aletem999@gmail.com>
2025-01-31 15:50:02 +0100

    Init A Java exercise Slot Machine

---
+++ src/Array/SlotMachine.java
@@ -0,0 +1,29 @@
+package Array;
+import java.util.Scanner;
+
+public class SlotMachine {
+    public static void main(String[] args) {
+
+//        Java Slot Machine
+
+        Scanner scanner = new Scanner(System.in);
+        int balance = 100;
+        int bet;
+        int payout;
+        String[] row;
+
+        System.out.println();
+        // Declare Variables
+        // Display Welcome Message
+        // Paly if Balance > 0
+        // Enter Bet Amount
+    //  Verify If Bet > Balance
+    //  Verify If Bet > 0
+    //  Substarct Bet From Balance
+    //  Spin Row
+    //  Print Row
+    //  get payOut
+    //  Ask >To play Again
+    //  Display Exit Message
+    }
+}
